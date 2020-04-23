d9f422440d81139b1365e9d12643599d5539db82
jtaylor6 <jarrede20@gmail.com>
2020-04-22 22:52:37 -0400

    Create players table.

--- db/setup.js
+++ db/setup.js
@@ -15,7 +15,27 @@ const createUserTable = () => {
         return console.log(response)
     })
 }
+const playerTables = () => {
+    const playerTableQuery = `CREATE TABLE IF NOT EXISTS players
+        (id SERIAL PRIMARY KEY,
+        email VARCHAR(50),
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        password VARCHAR(50),
+        date_created TIMESTAMP)`;
+
+    pool.query(playerTableQuery, [], (error, response) => {
+        if (error) {
+            throw new Error(error)
+        }
+        return console.log(response)
+    })
+}
+
+
 const createAllTables = () => {
     createUserTable()
+    playerTables()
 }
 createAllTables();
+
