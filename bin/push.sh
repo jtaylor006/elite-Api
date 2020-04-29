410652e411fffe03552260f3e153d326d68c5294
jtaylor6 <jarrede20@gmail.com>
2020-04-28 22:12:33 -0400

    created bin script

---
+++ bin/push.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+git add .
+git add -u
+read -r -p 'Commit message: ' desc  # prompt user for commit message
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+git commit -m "$desc "
+git push origin $branch
+
+# keep line 1for any bash script
