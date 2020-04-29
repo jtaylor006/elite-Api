72aeb446f19a47ec5e6ed88531a0cd3d80027723
jtaylor6 <jarrede20@gmail.com>
2020-04-28 22:02:57 -0400

    created helper function for formatting dates

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,5 @@
 const pool = require('./index.js');
-const getFormattedDate = (date) => {
-    const year = date.getFullYear();
-    let month = (1 + date.getMonth()).toString();
-    month = month.length > 1 ? month : "0" + month;
-    let day = date.getDate().toString();
-    day = day.length > 1 ? day : "0" + day;
-    return month + "/" + day + "/" + year;
-};
+const getFormattedDate = require ('../helpers/getFormattedDate')
 const formattedDate = getFormattedDate(new Date())
 
 const seedUserTable = () => {
