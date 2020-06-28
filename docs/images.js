ea4f3ecc0dc7fb2f3ea6df4efdecc2226cdfa153
Afam Olie <mikeolie@mikesImac.domain.com>
2020-06-27 21:07:36 -0700

    added api point for image

---
+++ docs/images.js
@@ -0,0 +1,40 @@
+/**
+ * @swagger
+ * /api/images:
+ *   post:
+ *     summary: Upload images to elite s3 bucket
+ *     description: Able to retrieve all of the stories
+ *     tags:
+ *       - Images
+ *     responses:
+ *       200:
+ *         description: Successfully uploaded image
+ *         content:
+ *            application/json:
+ *              schema:
+ *                  type: object
+ *                  description: Object describing the uploaded image
+ *                  properties:
+ *                      data:
+ *                          type: object
+ *                          description: Object returned by aws sdk describing the image
+ *                          properties:
+ *                              ETag:
+ *                                  type: string
+ *                                  description: Tag of uploaded image
+ *                              Location:
+ *                                  type: string
+ *                                  description: URL of uploaded image
+ *                              key:
+ *                                  type: string
+ *                                  description: Key of image
+ *                              Key:
+ *                                  type: string
+ *                                  description: Key of image
+ *                              Bucket:
+ *                                  type: string
+ *                                  description: Name of aws bucket the image was uploaded to
+ *       401:
+ *         description: Incorrect Authorization
+ *
+ */
